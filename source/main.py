56a0c2183cc58fbcab69d809e679afdb314142e1
Eric Kim <cyon256@icloud.com>
2026-03-16 00:41:10 +0900

    display surface

--- source/main.py
+++ source/main.py
@@ -4,6 +4,7 @@ import pygame
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Space Shooter")
 running = True
 
 def main():
@@ -12,8 +13,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        display_surface.fill((200,0,0))
+        pygame.display.update()
     pygame.quit()
-            
 
 if __name__ == "__main__":
     main()
