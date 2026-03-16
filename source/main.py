b4f91996294790ca4318eb8caab173a4593073dd
Eric Kim <cyon256@icloud.com>
2026-03-16 16:10:04 +0900

    basic_surf

--- source/main.py
+++ source/main.py
@@ -7,13 +7,19 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter")
 running = True
 
+#plain surface
+surf = pygame.Surface((100, 150))
+surf.fill("orange")
+
 def main():
+    
     global running
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        display_surface.fill((200,0,0))
+        display_surface.fill("darkgray")
+        display_surface.blit(surf, (100, 150))
         pygame.display.update()
     pygame.quit()
 
