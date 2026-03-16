9df73206fdaf9b1404fab56286e22da7baa3d84e
populate1356 <populate1356@gmail.com>
2026-03-17 08:12:21 +0900

    stars and ship

--- source/main.py
+++ source/main.py
@@ -1,4 +1,6 @@
 import pygame
+from os.path import join
+from random import randint
 
 
 pygame.init()
@@ -7,21 +9,36 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter")
 running = True
 
-#plain surface
-surf = pygame.Surface((100, 150))
-surf.fill("orange")
+
+# player surface
+player_path = join("images", "player.png")
+player_surf = pygame.image.load(player_path).convert_alpha()
+
+# star surface
+star_path = join("images", "star.png")
+star_surf = pygame.image.load(star_path).convert_alpha()
+
+star_positions = [
+    (randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for _ in range(14)
+]
+
 
 def main():
-    
+
     global running
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         display_surface.fill("darkgray")
-        display_surface.blit(surf, (100, 150))
+
+        for cord in star_positions:
+            display_surface.blit(star_surf, cord)
+
+        display_surface.blit(player_surf, (100, 150))
         pygame.display.update()
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
